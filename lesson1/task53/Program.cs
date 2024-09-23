d273dea60991b57bb20e313b94432009198d9fc9
Lukin Lukina <a.lukin@rambler.ru>
2024-09-23 22:28:00 +0300

    add task53

---
+++ lesson1/task53/Program.cs
@@ -0,0 +1,9 @@
+﻿//int numberA = 12;
+//int numberB = 5;
+//Console.WriteLine(numberA / numberB);
+// делит только целые числа без остатка
+
+double numberA = 12;
+double numberB = 5;
+Console.WriteLine(numberA / numberB);
+//делит с остатком
