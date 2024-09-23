b6218f5f0fef894256db2c213950229706317db2
Lukin Lukina <a.lukin@rambler.ru>
2024-09-23 23:32:48 +0300

    add task54

---
+++ lesson1/task54/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.Write("введите имя пользователя: ");
+string username = Console.ReadLine();
+
+//if(username =="Маша")
+if(username.ToLower() == "маша") //решение проблемы с регистром
+{
+    Console.WriteLine("Ура это же машка");
+}
+else
+{
+    Console.Write("привет, ");
+    Console.WriteLine(username);
+}
