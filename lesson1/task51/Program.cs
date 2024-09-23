0a41a44f609013ea423ebcb3c592e0b789dc956f
Lukin Lukina <a.lukin@rambler.ru>
2024-09-23 21:44:36 +0300

    add task51

---
+++ lesson1/task51/Program.cs
@@ -0,0 +1,10 @@
+﻿//Console.WriteLine("Введите ваше имя ");//
+//string username = Console.ReadLine();
+//Console.WriteLine("привет, ");
+//Console.WriteLine(username);
+
+
+Console.Write("Введите ваше имя ");
+string username = Console.ReadLine();
+Console.Write("привет, ");
+Console.Write(username);
