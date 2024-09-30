701c98d7a7404be8105bee87d25f12a94f7c8ad5
Lukin Lukina <a.lukin@rambler.ru>
2024-09-30 14:21:21 +0300

    task91

---
+++ task91/Program.cs
@@ -0,0 +1,16 @@
+﻿int[] arr = { 1, 12, 31, 18, 15, 16, 17, 18 };
+int n = arr.Length;
+int find = 18;
+
+int index = 0;
+
+while (index < n)
+{
+    if (arr[index] == find)
+    {
+        System.Console.WriteLine(index);
+        break; //если нужно найти только индекс только первого встретившегося элемента
+    }
+    index++;
+}
+//поиск индекса элемента
