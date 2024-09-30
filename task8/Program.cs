7f3e435dabcc7ee496b09c8d3286c2d6557d4a9c
Lukin Lukina <a.lukin@rambler.ru>
2024-09-30 10:35:17 +0300

    Task8

---
+++ task8/Program.cs
@@ -0,0 +1,12 @@
+﻿//int n = 10; // если arr.Length то эта строка не нужна
+int[] arr = { 2, 5, 4, 7, 8, 36, 1, 3, 8, 5 };
+int i = 0;
+
+while (i < arr.Length)
+{
+    if(arr[i] % 2 == 0)
+    {
+        System.Console.Write($"{arr[i]} ");
+    }
+    i = i + 1;
+}
