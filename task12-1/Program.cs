be3951fe43f5da0907760cfdcf2666308c2a7064
Lukin Lukina <a.lukin@rambler.ru>
2024-10-23 13:25:47 +0300

    task12-1new

--- task12-1/Program.cs
+++ task12-1/Program.cs
@@ -7,12 +7,13 @@ Method1();
 */
 
 // 
+/*
 void Method2(string msg)
 {
     System.Console.WriteLine(msg);
 }
 //Method2("текст сообщения");
-
+*/
 /*void Method21(string msg, int count)
 {
     int i =0;
@@ -38,7 +39,7 @@ System.Console.WriteLine(year);
 */
 
 // 4 вид. чтото принимают и чтото возвращают
-string Method4(int count, string c)
+/*string Method4(int count, string c)
 {
     int i = 0;
     string result = String.Empty;
@@ -52,3 +53,66 @@ string Method4(int count, string c)
 }
 string res = Method4(3, "ltbj");
 System.Console.WriteLine(res);
+*/
+// перерабатываем 4 вариант для цыкла for
+/*
+string Method4(int count, string text)
+{
+    string result = String.Empty;
+for (int i = 0; i < count; i++)
+    {
+        result = result + text;
+    }
+    return result;
+}
+string res = Method4(3, "ltbj");
+System.Console.WriteLine(res);
+*/
+
+// цикл в цикле - таблица умножения
+
+/*
+for (int i = 2; i <= 10; i++)
+{
+    for (int j = 2; j <= 10; j++)
+    {
+        System.Console.WriteLine($"{i} * {j} = {i*j}");
+    }
+    System.Console.WriteLine();
+}
+*/
+// пробел на черночку
+// к на К
+// С на с
+
+string text = " – Я думаю, – сказал князь, улыбаясь, – что, "
+            + " ежели бы вас послали вместо нашего милого Винценгероде, "
+            + " вы бы взяли приступом согласие прусского короля. "
+            + " Вы так красноречивы. Вы дадите мне чаю? ";
+
+// string s = "qwerty"
+//             012
+// s[3] // r
+
+string Replace(string text, char oldValue, char newValue)
+{
+    string result = String.Empty;
+    int length = text.Length;
+    for (int i = 0; i < length; i++)
+    {
+        if(text[i] == oldValue) result = result + $"{newValue}";
+        else result = result + $"{text[i]}";
+    }
+
+    return result;
+}
+
+string newText = Replace(text, ' ', '|');
+System.Console.WriteLine(newText);
+System.Console.WriteLine();
+newText = Replace(newText, 'к', 'К');
+System.Console.WriteLine(newText);
+
+System.Console.WriteLine();
+newText = Replace(newText, 'с', 'С');
+System.Console.WriteLine(newText);
